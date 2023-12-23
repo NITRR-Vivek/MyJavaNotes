d4bacf5859e38014703e7a2788a46b402ea7d7ed
NITRR-Vivek <vivekkumarpaneer3@gmail.com>
2023-12-23 18:43:29 +0530

    added static function

--- StaticFunction.java
+++ StaticFunction.java
@@ -1,16 +1,21 @@
 class Area {
-static void fun1(){
-System.out.println("Static Function of other class");
+	int x;
+	private static int y;
+void fun1(){
+	x=10;
+	y=15;
+	System.out.println(x);
 }
 static void fun2(){
-System.out.println("Area 2");
+	System.out.println(y);
 }}
 class StaticFunction {
 static void fun1 (){
-System.out.println("C");
+	System.out.println("20");
 }
 public static void main (String []args){
-Area.fun1();
-Area.fun2();
-fun1();
+	Area a1 = new Area();
+	a1.fun1();
+	Area.fun2();
+	fun1();
 }}
