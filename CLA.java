d87db84a4fb1f6acb0610338cf2ddb3f94281ec7
NITRR-Vivek <vivekkumarpaneer3@gmail.com>
2023-12-26 20:05:09 +0530

    added Command Line Arguments

---
+++ CLA.java
@@ -0,0 +1,10 @@
+// command line arguments (CLA)
+// ex in cmd : java CLA amit mahesh , these arguments will be taken by the main function.
+class CLA {
+public static void main(String []args){
+	
+	for(int i = 0; i < args.length;i++){
+		System.out.println(args[i]);
+	} 
+}
+}
